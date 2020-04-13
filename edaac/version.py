05b1f1e084818d0a5dac73059e21584bdc6abfc4
Abdelrahman <abdelrahman@brown.edu>
2020-04-13 02:28:20 -0400

    bump version

--- edaac/version.py
+++ edaac/version.py
@@ -17,7 +17,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 
 def version():
     print("EDA Analytics Central (EDAAC) v" + __version__)
